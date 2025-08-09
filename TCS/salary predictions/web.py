5aa563aa613785333bbaf1a36d8cd14584b66def
23f3000896 <23f3000896@ds.study.iitm.ac.in>
2025-08-09 16:01:32 +0530

    Add files via upload

---
+++ TCS/salary predictions/web.py
@@ -0,0 +1,44 @@
+'''from flask import Flask,render_template,request
+import pickle
+import numpy as np
+
+app=Flask(__name__)
+model=pickle.load(open("model.pkl","rb"))
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+    #return redirect(url_for("predict"))
+@app.route('/predict', methods=['POST'])
+def predict():
+    features = [float(x) for x in request.form.values()]
+    final_features = [np.array(features)]
+    output = model.predict(final_features)
+    return render_template('res.html', prediction_text="The Salary is Rs {:.2f}".format(output[0]))
+
+if __name__ =='__main__':
+    app.run(debug=True)'''
+
+import pickle
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+data = pd.read_csv(r"C:\Users\user\Desktop\TCS\salary prediction\HRDataset.csv")
+
+# Choose relevant features, excluding EmpID
+features = ['GenderID', 'DeptID', 'PerfScoreID']
+# You can expand this list with features like 'EngagementSurvey', 'EmpSatisfaction', etc.
+
+X = data[features]
+y = data['Salary']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+print(y_pred)
+
+pickle.dump(regressor, open("model.pkl", "wb"))
